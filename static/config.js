c0a7435ed56d28dc468b73bfd4cc549c39ef4d62
li8621214991 <2937063434@qq.com>
2019-10-16 10:39:01 +0800

    guizhou

--- static/config.js
+++ static/config.js
@@ -29,7 +29,7 @@ window.config={
     },
     //报修
     person_repair:{
-        url:url+name+'/weixin/wuye/index.html?state=123#/repair',//报修
+        url:url+name+'/weixin/wuye/index.html?state=123#/repair?projectId=1',//报修
         fuwux:url+name+'/weixin/wuye/index.html?state=123#/butler?category=0',//服务需求 
         butler:url+name+'/weixin/wuye/index.html?state=123#/mysteward',//建议
     },
